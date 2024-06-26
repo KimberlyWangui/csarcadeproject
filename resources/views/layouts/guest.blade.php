1473f95e3d29051bc2f2f77f0cafd37cb0d4e80c
Kimberly Wangui <kimberly.wangui@strathmore.edu>
2024-06-26 10:09:44 +0300

    Updated Sign in and sign up forms

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,17 +14,11 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <body class="font-sans antialiased">
+        <div class="flex flex-col min-h-screen w-full">
+            <main class="flex-grow w-full">
                 {{ $slot }}
-            </div>
+            </main>
         </div>
     </body>
 </html>
