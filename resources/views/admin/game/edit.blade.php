2ef3591a95adba01fb9c49d44f3350498c558dcc
Kimberly Wangui <kimberly.wangui@strathmore.edu>
2024-07-15 13:31:39 +0300

    Modified Edit Pages

--- resources/views/admin/game/edit.blade.php
+++ resources/views/admin/game/edit.blade.php
@@ -7,43 +7,42 @@
 @section('content')
 
 <div class="row">
-        <div class="col-12">
-          <div class="card my-4">
+    <div class="col-12">
+        <div class="card my-4">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                <h6 class="text-white text-capitalize ps-3">Games Edit Data</h6>
-              </div>
-              <form action="/admin/game-edit/{{$games->game_id}}" method="POST">
-                {{ csrf_field() }}
-                {{ method_field('PUT') }}
-               <div class="modal-body">
-        
-                <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Name:</label>
-            <input type="text" name="name" class="form-control" value="{{$games->name}}">
-          </div>
-          <div class="mb-3">
-          <label for="recipient-name" class="col-form-label">Videopath:</label>
-          <input type="text" name="video_path" class="form-control" value="{{$games->video_path}}">
-          </div>
-          <div class="mb-3">
-            <label for="message-text" class="col-form-label">Description:</label>
-            <textarea name="description" class="form-control">{{$games->description}}</textarea>
-          </div>
-      </div>
-      <div class="modal-footer">
-        <a href="/admin/games" class="btn btn-secondary">BACK</button>
-        <button type="submit" class="btn btn-primary">UPDATE</button>
-      </div>
-      </form>
+                <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                    <h6 class="text-white text-capitalize ps-3">Games Edit Data</h6>
+                </div>
             </div>
+            <div class="card-body px-0 pb-2">
+                <form action="/admin/game-edit/{{$games->game_id}}" method="POST">
+                    {{ csrf_field() }}
+                    {{ method_field('PUT') }}
+                    
+                    <div class="form-group">
+                        <label class="font-weight-bold">NAME:</label>
+                        <input type="text" name="name" value="{{$games->name}}" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="font-weight-bold">VIDEO PATH:</label>
+                        <input type="text" name="video_path" value="{{$games->video_path}}" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="font-weight-bold">DESCRIPTION:</label>
+                        <textarea name="description" class="form-control">{{$games->description}}</textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">UPDATE</button>
+                    <a href="/admin/games" class="btn btn-secondary">BACK</a>
+                </form>
             </div>
         </div>
+    </div>
 </div>
 
-     
 @endsection
 
 @section('scripts')
 @endsection
-
--- resources/views/admin/promotion/edit.blade.php
+++ resources/views/admin/promotion/edit.blade.php
@@ -5,48 +5,46 @@
 @endsection
 
 @section('content')
+
 <div class="row">
-        <div class="col-12">
-          <div class="card my-4">
+    <div class="col-12">
+        <div class="card my-4">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                <h6 class="text-white text-capitalize ps-3">Promotion Edit Data</h6>
-
-                
-        <form action="/admin/promotion-update/{{$PromotionCode->id}}" method="POST">
-        {{ csrf_field() }}
-        {{ method_field('PUT') }}
-
-      <div class="modal-body">
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Code:</label>
-            <input type="text" name="code" class="form-control" id="recipient-name" value="{{$PromotionCode->code}}">
-          </div>
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Discount Percentage:</label>
-            <input type="text" name="discount_percentage" class="form-control" id="recipient-name" value="{{$PromotionCode->discount_percentage}}">
-          </div>
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">First Time Only:</label>
-            <input type="text" name="first_time_only" class="form-control" id="recipient-name" value="{{$PromotionCode->first_time_only}}">
-          </div>
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Minimum Cart Value:</label>
-            <input type="text" name="minimum_cart_value" class="form-control" id="recipient-name" value="{{$PromotionCode->minimum_cart_value}}">
-          </div>
-      </div>
-      <div class="modal-footer">
-        <a href="/admin/promotion" class="btn btn-secondary">BACK</button>
-        <button type="submit" class="btn btn-primary">UPDATE</button>
-      </div>
-      </form>
-
-              </div>
+                <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                    <h6 class="text-white text-capitalize ps-3">Promotion Edit Data</h6>
+                </div>
             </div>
-          </div>
-        </div>
-</div>
+            <div class="card-body px-0 pb-2">
+                <form action="/admin/promotion-update/{{$PromotionCode->id}}" method="POST">
+                    {{ csrf_field() }}
+                    {{ method_field('PUT') }}
+                    
+                    <div class="form-group">
+                        <label for="code" class="font-weight-bold">CODE:</label>
+                        <input type="text" name="code" class="form-control" id="code" value="{{$PromotionCode->code}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="discount_percentage" class="font-weight-bold">DISCOUNT PERCENTAGE:</label>
+                        <input type="text" name="discount_percentage" class="form-control" id="discount_percentage" value="{{$PromotionCode->discount_percentage}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="first_time_only" class="font-weight-bold">FIRST TIME ONLY:</label>
+                        <input type="text" name="first_time_only" class="form-control" id="first_time_only" value="{{$PromotionCode->first_time_only}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="minimum_cart_value" class="font-weight-bold">MINIMUM CART VALUE:</label>
+                        <input type="text" name="minimum_cart_value" class="form-control" id="minimum_cart_value" value="{{$PromotionCode->minimum_cart_value}}">
+                    </div>
 
+                    <div class="modal-footer">
+                        <a href="/admin/promotion" class="btn btn-secondary">BACK</a>
+                        <button type="submit" class="btn btn-primary">UPDATE</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
 
