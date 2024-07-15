695261ab15a8ed3e2d59ab4bec26f89b6535c22f
Kimberly Wangui <kimberly.wangui@strathmore.edu>
2024-07-15 08:37:00 +0300

    Admin dashboard changes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ticket;
+use App\Models\Game;
+use App\Models\PromotionCode;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $PromotionCodes = PromotionCode::all();
+        $Games = Game::all();
+        $Tickets = Ticket::all();
+        return view('admin.dashboard')
+        ->with('Tickets', $Tickets)
+        ->with('Games', $Games)
+        ->with('PromotionCodes', $PromotionCodes);
     }
 }
